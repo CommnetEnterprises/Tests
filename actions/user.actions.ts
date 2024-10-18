96c5ea25210e0125273728a33d3cfa6c07016dc0
Lettimaks <lethabolesheleba2003@gmail.com>
2024-10-18 20:33:54 +0200

    Init mgr

---
+++ actions/user.actions.ts
@@ -0,0 +1,201 @@
+"use server";
+
+import { FilterQuery, SortOrder } from "mongoose";
+import { revalidatePath } from "next/cache";
+
+import Community from "../models/community.model";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+
+import { connectToDB } from "../mongoose";
+
+(async () => {
+  try {
+    await connectToDB();
+    // Proceed with database operations, e.g., fetchUser, updateUser, etc.
+  } catch (error) {
+    console.error("Database initialization failed:", error);
+    process.exit(1); // Exit the application if the database connection fails
+  }
+})();
+
+export async function fetchUser(userId: string) {
+  try {
+    connectToDB();
+
+    return await User.findOne({ id: userId }).populate({
+      path: "communities",
+      model: Community,
+    });
+  } catch (error: unknown) {
+    // Handle the error more safely
+    if (error instanceof Error) {
+      throw new Error(`Failed to fetch user: ${error.message}`);
+    } else {
+      throw new Error("Failed to fetch user: Unknown error occurred");
+    }
+  }
+}
+
+interface Params {
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+}
+
+export async function updateUser({
+  userId,
+  bio,
+  name,
+  path,
+  username,
+  image,
+}: Params): Promise<void> {
+  try {
+    connectToDB();
+
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded: true,
+      },
+      { upsert: true }
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: unknown) {
+    // Handle the error more safely
+    if (error instanceof Error) {
+      throw new Error(`Failed to create/update user: ${error.message}`);
+    } else {
+      throw new Error("Failed to create/update user: Unknown error occurred");
+    }
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: [
+        {
+          path: "community",
+          model: Community,
+          select: "name id image _id",
+        },
+        {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "name image id",
+          },
+        },
+      ],
+    });
+    return threads;
+  } catch (error: unknown) {
+    console.error("Error fetching user threads:", error);
+    if (error instanceof Error) {
+      throw new Error(`Error fetching user threads: ${error.message}`);
+    } else {
+      throw new Error("Error fetching user threads: Unknown error occurred");
+    }
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const regex = new RegExp(searchString, "i");
+
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId },
+    };
+
+    if (searchString.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+    const users = await usersQuery.exec();
+
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error: unknown) {
+    console.error("Error fetching users:", error);
+    if (error instanceof Error) {
+      throw new Error(`Error fetching users: ${error.message}`);
+    } else {
+      throw new Error("Error fetching users: Unknown error occurred");
+    }
+  }
+}
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    const userThreads = await Thread.find({ author: userId });
+
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, [] as string[]);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error: unknown) {
+    console.error("Error fetching replies: ", error);
+    if (error instanceof Error) {
+      throw new Error(`Error fetching replies: ${error.message}`);
+    } else {
+      throw new Error("Error fetching replies: Unknown error occurred");
+    }
+  }
+}
